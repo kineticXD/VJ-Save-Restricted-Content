8fe8c34e59665b21e1968c4d7d933403326a6751
kineticXD <113818020+kineticXD@users.noreply.github.com>
2026-07-21 18:58:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ else:
     STRING_SESSION = None
 
 # Bot token @Botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8980439596:AAHwA5C75UR7ccr0kg9D_RiSIyAQubPDgPI")
 
 # Your API ID from my.telegram.org
 API_ID = int(os.environ.get("API_ID", "33092002"))
