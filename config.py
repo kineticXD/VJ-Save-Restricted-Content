45f28ca479d13e3bc1f386e50194b4197c7bef7c
kineticXD <113818020+kineticXD@users.noreply.github.com>
2026-07-21 22:14:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ CHANNEL_ID = os.environ.get("CHANNEL_ID", "")
 
 # Your Mongodb Database Url
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = os.environ.get("DB_URI", "mongodb+srv://BotUser:BotUserPassword@cluster0.s2q0pk9.mongodb.net/") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://BotUser:BotUserPassword@cluster0.s2q0pk9.mongodb.net/vj_bot_db?retryWrites=true&w=majority") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_NAME = os.environ.get("DB_NAME", "vjsavecontentbot")
 
 # Increase time as much as possible to avoid floodwait, spamming and tg account ban issues.
